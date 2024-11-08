366b961a81de6bd4d497d1e74e02beea8c4b6f51
monish <monishsharma080@gmail.com>
2024-11-08 22:49:42 +0530

    write a test(2)

--- a_test.go
+++ a_test.go
@@ -8,16 +8,17 @@ import (
 
 func TestGetTheSubtitles(t *testing.T) {
 	// func get_the_subtitles(httpClient http.Client, youtubeUrl string, want_text_without_time bool, channel_for_subtitles chan<- string_and_error_channel) {
-	a:= time.Now()
+	
 	httP_client_1 := http.Client{}
-	youtubeUrl := "https://www.youtube.com/watch?v=DjBrLbOJXHA"
+	youtubeUrl := "https://www.youtube.com/watch?v=YLuZjpxmsZQ"
 	want_text_without_time := true
 	channel_for_subtitles := make(chan string_and_error_channel)
 	println("sleeping")
-	
-	time.Sleep(3000)
+
+	time.Sleep(30000)
 
 	println("finished sleeping")
+	a:= time.Now()
 	go Get_the_subtitles(httP_client_1, youtubeUrl, want_text_without_time, channel_for_subtitles)
 	result := <- channel_for_subtitles
 	if result.err != nil {
