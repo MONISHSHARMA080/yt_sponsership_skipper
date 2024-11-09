5b1b0dd3a7049e9c22c865e47a7d33ed724b42d7
monish <monishsharma080@gmail.com>
2024-11-09 09:56:48 +0530

    hopefully fix

--- my_yt.go
+++ my_yt.go
@@ -35,6 +35,24 @@ type userForDB struct {
 }
 
 func main() {
+	
+		httP_client_1 := http.Client{}
+		youtubeUrl := "https://www.youtube.com/watch?v=sS6u5UU3t3c"
+		want_text_without_time := true
+		channel_for_subtitles := make(chan string_and_error_channel)
+		println("sleeping")
+	
+		time.Sleep(30000)
+	
+		println("finished sleeping")
+		a:= time.Now()
+		go Get_the_subtitles(httP_client_1, youtubeUrl, want_text_without_time, channel_for_subtitles)
+		result := <- channel_for_subtitles
+		if result.err != nil {
+			print("error ocurred -->"+result.err.Error()+"\n")
+		}
+		print("\n\n",result.string_value+"<<===,,,result value was this \n\n")
+		print(time.Since(a).Seconds())
 	// almost the base is done , now I should start assembling the pieces together
 	// what would that be >>  api  routes
 	// >> concurrency, ---doing this
@@ -50,23 +68,8 @@ func main() {
 	
 
 
-	httP_client_1 := http.Client{}
-	youtubeUrl := "https://www.youtube.com/watch?v=sS6u5UU3t3c"
-	want_text_without_time := true
-	channel_for_subtitles := make(chan string_and_error_channel)
-	println("sleeping")
 
-	time.Sleep(30000)
 
-	println("finished sleeping")
-	a:= time.Now()
-	go Get_the_subtitles(httP_client_1, youtubeUrl, want_text_without_time, channel_for_subtitles)
-	result := <- channel_for_subtitles
-	if result.err != nil {
-		print("error ocurred -->"+result.err.Error()+"\n")
-	}
-	print("\n\n",result.string_value+"<<===,,,result value was this \n\n")
-	print(time.Since(a).Seconds())
 	encryption_key := os.Getenv("encryption_key")
 	encryption_key_as_byte := []byte(os.Getenv("encryption_key"))
 
