cbd170c07249b240f3d9d40e8085dd0c644833b4
monish <monishsharma080@gmail.com>
2024-11-13 13:24:26 +0530

    made a random api generator based on the user's teir and almost done with  this

--- structs_for_helperfile2.go
+++ structs_for_helperfile2.go
@@ -9,6 +9,7 @@ type String_and_error_channel_for_groq_response struct {
 	http_response_for_go_api_ptr *http.Response
 }
 
-func (groqResp *String_and_error_channel_for_groq_response) f (){
-	
+func (groqResp *String_and_error_channel_for_groq_response) checkIfSponsorshipSubtitlesExist (transcript *Transcripts) (bool,int64,error){
+	//will check the groq response and if the ads is present (in the groq resp bool) then I will go through the transcript to find it and return the time to the user
+	return false, 0, nil
 }
