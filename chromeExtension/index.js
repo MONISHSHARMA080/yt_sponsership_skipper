a0b8aba69a4104b9e9f122e0c30c149b10803c21
MONISHSHARMA080 <monishsharma080@gmail.com>
2024-12-15 13:48:16 +0530

    got the email and working on getting the authToken

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,8 +1,25 @@
 console.log("hi form the index.js");
+
 try {
-  document.getElementById("status").addEventListener("click", function a() {
-    console.log("hi from the index.js button-- ");
-  });
+  let element = document.getElementById("status");
+  if (element !== null) {
+    element.addEventListener("click", function a() {
+      console.log("hi from the index.js button-- ");
+      getUserDetailsForSignUp();
+    });
+  }
 } catch (e) {
   console.log(e);
+}
+
+function getUserDetailsForSignUp() {
+  chrome.identity.getProfileUserInfo({}, (userInfo) => {
+    console.log("email I got is -->", userInfo.email);
+    console.log("id I got is -->", userInfo.id);
+    console.log("whole obj-->", userInfo);
+  });
+  chrome.identity.getProfileUserInfo({}, (ProfileUserInfo) => {
+    console.log("profile user info is ", ProfileUserInfo);
+  });
+  chrom.identity.getAuthToken({}, (GetAuthTokenResult) => {});
 }
