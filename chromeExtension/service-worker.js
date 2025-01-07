6d8d1f325e3ded753f2db40af5e6cd3e97aa4309
MONISHSHARMA080 <monishsharma080@gmail.com>
2025-01-07 12:50:51 +0530

    import working

--- chromeExtension/service-worker.js
+++ chromeExtension/service-worker.js
@@ -1,4 +1,9 @@
-import { sayHi } from './helper.js';
+import { sayHi, getKeyFromStorageOrBackend } from './helper.js';
 
 console.log("hi from the service worker and will run say hi() now");
 sayHi();
+async  function sayHi1() {
+
+    await getKeyFromStorageOrBackend()
+}
+sayHi1().then(r => console.log("value is r in " ,r)).catch(e => console.log("error is -->",e));
