5772c7e740e8e7fe40a399c8dd43e3b722420cfb
MONISHSHARMA080 <monishsharma080@gmail.com>
2025-04-02 11:59:33 +0530

    made changes that will write the shared key object to the storage when we make any changes to it; now the problem is that when the payment is completed the isPaidUser is being set to true and when we refresh it the is again being set to false

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -7,92 +7,117 @@
 	import { checkIfKeyIsValidAndUpdateTheState } from '$lib/utils/seeIfTheKeyIsValidByBackend';
 	import { askBackendForOrderId } from '$lib/utils/razorpayIntegration/AskBackendForOrderId';
 	import { razorpayOrderId } from '$lib/sharedState/razorPayKey.svelte';
-	import {  keyUpdatedState } from '$lib/sharedState/updatedKeyReceived.svelte';
-	
-		// Commented extension code preserved as in original
-		// let interactWithExtensionClass = new interactWithTheChromeExtensionAndStoreIt
-		// let error = interactWithExtensionClass.start((key)=>{console.log("the key is received and it is ->",key," --- about to update the svelete store")
-		// 	// keyFromChromeExtensionState.key = key
-		// 	interactWithExtensionClass.cleanup()
-		// let checkKeyAndnew = new checkIfKeyIsValidAndUpdateTheState()
-		// //  checkKeyAndnew.seeIfKeyIsValid(key)
-		// }
-		// console.log("error in interacting with the chrome extension is -> ",error );
-		// const sendNewKeyClass = new sendChromeExtensionNewKey("(((((((((((((((((((((((((((((((")
-		// sendNewKeyClass.sendKey().then((response)=>{
-		// 	console.log("the response after sending the key is  ->",response)
-		// 	console.log('\n\n\n\n');
-		// 	// sendNewKeyClass.clearEventListener()
-		// })
+	import { keyUpdatedState } from '$lib/sharedState/updatedKeyReceived.svelte';
+	import { WriteSharedStateToStorageWhenItChanges } from '$lib/utils/SharedState/WriteSharedStateToLocalStorageOnChange.svelte';
 
+	// Commented extension code preserved as in original
+	// let interactWithExtensionClass = new interactWithTheChromeExtensionAndStoreIt
+	// let error = interactWithExtensionClass.start((key)=>{console.log("the key is received and it is ->",key," --- about to update the svelete store")
+	// 	// keyFromChromeExtensionState.key = key
+	// 	interactWithExtensionClass.cleanup()
+	// let checkKeyAndnew = new checkIfKeyIsValidAndUpdateTheState()
+	// //  checkKeyAndnew.seeIfKeyIsValid(key)
+	// }
+	// console.log("error in interacting with the chrome extension is -> ",error );
+	// const sendNewKeyClass = new sendChromeExtensionNewKey("(((((((((((((((((((((((((((((((")
+	// sendNewKeyClass.sendKey().then((response)=>{
+	// 	console.log("the response after sending the key is  ->",response)
+	// 	console.log('\n\n\n\n');
+	// 	// sendNewKeyClass.clearEventListener()
+	// })
 
-		
-		onMount(()=>{
-			let interactWithExtensionClass = new interactWithTheChromeExtensionAndStoreIt
-		let error =interactWithExtensionClass.start(
+	onMount(() => {
+		let interactWithExtensionClass = new interactWithTheChromeExtensionAndStoreIt();
+		let error = interactWithExtensionClass.start(
 			// func that will run after we get the keys form the chrome extension and not form the local storage
-			(a)=>{console.log("the key we got in the func passed in the start() is", a);
-			keyFromChromeExtensionState.key = a
-			interactWithExtensionClass.cleanup()
-		})
+			(a) => {
+				console.log('the key we got in the func passed in the start() is', a);
+				keyFromChromeExtensionState.key = a;
+				interactWithExtensionClass.cleanup();
+			}
+		);
 		if (error) {
 			console.log(`the error in interacting with the extension class is ->${error}`);
 		}
-		console.log("about to ask backend for the order id");
-		
-			 askBackendForOrderId(keyFromChromeExtensionState).then((val)=>{
-				console.log(`the order id form the backend's value is ->${val}`);
-				
-			 })
+		console.log('about to ask backend for the order id');
 
+		askBackendForOrderId(keyFromChromeExtensionState).then((val) => {
+			console.log(`the order id form the backend's value is ->${val}`);
+		});
+	});
+	// let error = interactWithExtensionClass.start((key)=>{console.log("the key is received and it is ->",key," --- about to update the svelete store")
 
+	let val = $derived(keyFromChromeExtensionState);
+	$effect(() => {
+		console.log(
+			`asking backend for the order id and the key is -> ${keyFromChromeExtensionState.key}`
+		);
+		askBackendForOrderId(val).then((val) => {
+			console.log(`the svelte effect returned and the value is ->`, val);
+		});
+	});
+
+	// 	console.log(`\n\n\n----++----this log is form the plain log and not form effect when the keyFromChromeExtensionState changes val
+	// -->${JSON.stringify(val)}`);
+
+	// if we don't do this and the ordered id is used in a failure , it will not update, also update it only when the failure is registered
+	// let newRazorPayOrderID = $derived(razorpayOrderId)
+	// $effect(()=>{
+	// 	console.log(`the razor pay order id after change is -> ${JSON.stringify(newRazorPayOrderID)}`);
+	// 	if (newRazorPayOrderID.numberOfTimesKeyUsed === 1 || newRazorPayOrderID.numberOfTimesKeyUsed >1) {
+	// 		console.log(`the razor pay ordered id has been used one time`);
+	// 		askBackendForOrderId(keyFromChromeExtensionState).then((val)=>{
+	// 			console.log(`the razorpay order ID is used one time and we are updated it and the returned value is  -> ${val} `);
+	// 		})
+	// 	}else{
+	// 		return
+	// 	}
+	// })
+
+	let keyUpdatedObj = $derived(keyUpdatedState);
+	$effect(() => {
+		if (keyUpdatedObj.newKeyReceived && keyFromChromeExtensionState.key) {
+			// the key is updated and we are sending it to the chrome extension
+			const sendNewKeyClass = new sendChromeExtensionNewKey(keyFromChromeExtensionState.key);
+			sendNewKeyClass.sendKey().then(([error, didWeCorrectlySendIt]) => {
+				console.log(
+					`did we correctly send the new key to the chrome extension ->${didWeCorrectlySendIt}, and errors here is ->${error}`
+				);
+				if (error === null) {
+					keyUpdatedObj.newKeyReceived = false;
+				}
+				sendNewKeyClass.clearEventListener();
+			});
 		}
-	)
-		// let error = interactWithExtensionClass.start((key)=>{console.log("the key is received and it is ->",key," --- about to update the svelete store")
+	});
 
-		
-		let val = $derived(keyFromChromeExtensionState)
-		$effect(()=>{
-			console.log(`asking backend for the order id and the key is -> ${keyFromChromeExtensionState.key}`);
-				 askBackendForOrderId(val).then((val)=>{
-				console.log(`the svelte effect returned and the value is ->`, val);
-			 })
-		})
+	$effect(() => {
+		console.log(
+			`the razor pay id is ->`,
+			razorpayOrderId.orderIdForOnetime,
+			razorpayOrderId.orderIdForRecurring
+		);
+	});
 
-		// if we don't do this and the ordered id is used in a failure , it will not update, also update it only when the failure is registered
-		// let newRazorPayOrderID = $derived(razorpayOrderId)
-		// $effect(()=>{
-		// 	console.log(`the razor pay order id after change is -> ${JSON.stringify(newRazorPayOrderID)}`);
-		// 	if (newRazorPayOrderID.numberOfTimesKeyUsed === 1 || newRazorPayOrderID.numberOfTimesKeyUsed >1) {
-		// 		console.log(`the razor pay ordered id has been used one time`);
-		// 		askBackendForOrderId(keyFromChromeExtensionState).then((val)=>{
-		// 			console.log(`the razorpay order ID is used one time and we are updated it and the returned value is  -> ${val} `);
-		// 		})
-		// 	}else{
-		// 		return
-		// 	}
-		// })
-
-		let keyUpdatedObj = $derived(keyUpdatedState)
-		$effect(()=>{
-			if (keyUpdatedObj.newKeyReceived && keyFromChromeExtensionState.key ){
-				// the key is updated and we are sending it to the chrome extension
-				const sendNewKeyClass = new sendChromeExtensionNewKey(keyFromChromeExtensionState.key)
-				sendNewKeyClass.sendKey().then(([error, didWeCorrectlySendIt])=>{
-					console.log(`did we correctly send the new key to the chrome extension ->${didWeCorrectlySendIt}, and errors here is ->${error}`);
-					if (error === null){
-						keyUpdatedObj.newKeyReceived = false
-					}
-						sendNewKeyClass.clearEventListener()
-				})
-			}
-		})
-
-		$effect(()=>{
-			console.log(`the razor pay id is ->`, razorpayOrderId.orderIdForOnetime, razorpayOrderId.orderIdForRecurring);
-		})
-
-
-  
+	// WriteSharedStateToStorageWhenItChanges
+	// write the keyFromChromeExtensionState in the storage when it changes
+	let writeTolocalStorageInChnage = new WriteSharedStateToStorageWhenItChanges(
+		keyFromChromeExtensionState,
+		'KEY'
+	);
+	let err = writeTolocalStorageInChnage.wathcAndSaveOnChange((sharedStateObj) => {
+		if (sharedStateObj.key === '' || sharedStateObj.key === null) {
+			return false;
+		} else {
+			console.log(`about to return false and the key is ${sharedStateObj.key}`);
+			return true;
+		}
+	});
+	if (err instanceof Error) {
+		console.log(
+			`there is a error in the writtign the state to the local storage and it is ${err} `
+		);
+	}
 </script>
-   <Component14 /> 
+
+<Component14 />
