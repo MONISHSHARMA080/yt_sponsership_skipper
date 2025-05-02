2b61d4221ffd24d3ebd7e180b862ca6134f74181
MONISHSHARMA080 <monishsharma080@gmail.com>
2025-05-02 22:34:48 +0530

    made some printing decision for better reading

--- tests/helperPackages/extension/networkResponse.go
+++ tests/helperPackages/extension/networkResponse.go
@@ -95,7 +95,7 @@ func (ce ChromeExtension) GetResponseFromServerToChromeExtension(ctx context.Con
 			}
 			// Filter for API calls related to your extension
 			fmt.Printf("response from the networkEventsChan is %+v \n\n", resp)
-			fmt.Printf("response from the networkEventsChan is %+v \n\n", *resp)
+			fmt.Printf("response is -> %+v \n\n", *resp.Response)
 			println("the response url is ->", resp.Response.URL)
 			if strings.Contains(resp.Response.URL, "localhost") ||
 				strings.Contains(resp.Response.URL, "/youtubeVideo") {
