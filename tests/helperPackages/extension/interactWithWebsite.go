a56fe997c086dcbbf47da30dcf26a4b5c44b6b84
Monish Sharma <107314033+MONISHSHARMA080@users.noreply.github.com>
2025-05-03 11:34:25 +0000

    only one test failled

--- tests/helperPackages/extension/interactWithWebsite.go
+++ tests/helperPackages/extension/interactWithWebsite.go
@@ -27,7 +27,7 @@ func (ce *ChromeExtension) MakeThePaymentAndGetOnPaidTier(ctx context.Context, s
 	tryOnceButton := `bg-yellow-500 text-black flex w-full transform items-center justify-center rounded-md border-3 border-black px-8 py-3 font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[2px] hover:translate-y-[2px] hover:shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] active:translate-x-[4px] active:translate-y-[4px] active:shadow-none s-N59DAw_bCMv0`
 	goPremiumButton := `bg-purple-600 text-white flex w-full transform items-center justify-center rounded-md border-3 border-black px-8 py-3 font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[2px] hover:translate-y-[2px] hover:shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] active:translate-x-[4px] active:translate-y-[4px] active:shadow-none s-N59DAw_bCMv0`
 	err = chromedp.Run(ctx,
-		chromedp.WaitVisible(tryOnceButton),
+		chromedp.WaitVisible(tryOnceButton, chromedp.BySearch),
 	)
 	println("we have waited for the button to be visible and now we will click on it, hopefully it is visible")
 	if err != nil {
