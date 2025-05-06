84629409cf586b300d5676e94cd5409874cfc4c4
MONISHSHARMA080 <monishsharma080@gmail.com>
2025-05-06 18:43:34 +0530

    added vercel track for analytics

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import '../app.css';
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { dev } from '$app/environment';
 	injectSpeedInsights();
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 	let { children } = $props();
 </script>
 
