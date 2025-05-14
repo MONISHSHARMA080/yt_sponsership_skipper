6ba64de4cea13ea8327b82399151587e6cc38a3a
MONISHSHARMA080 <monishsharma080@gmail.com>
2025-05-14 14:47:08 +0530

    changed  the chrome extension's default backend url to be prod

--- chromeExtension/config.js
+++ chromeExtension/config.js
@@ -1,7 +1,6 @@
 export const config = {
-  // BACKEND_URL: "https://ytsponsershipskipper-production.up.railway.app",
-  BACKEND_URL: "http://localhost:8080",
+  BACKEND_URL: "https://ytsponsershipskipper-production.up.railway.app",
+  // BACKEND_URL: "http://localhost:8080",
 
-  // BACKEND_URL: "https://ytsponsershipskipper-production-ec3e.up.railway.app",
   websiteURL: "https://youtube-sponsership-skipper.vercel.app",
 };
